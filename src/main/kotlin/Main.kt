fe81b3bf171c7779cb3b81c972e7452425bcc9d9
Ahmad <totsukamail3278@gmail.com>
2022-06-12 13:26:22 +0700

    modified style and more

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,26 +2,31 @@ import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.system.exitProcess
 
-const val APP = "Todolist App"
+const val APP = "Todolist-Application"
 const val VERSION = "0.0.1"
-val todolists = arrayListOf<String>()
+val todolist = arrayListOf<String>()
 
 fun main(){
     showMenus()
 }
 
-fun showMenus(){
+fun showMenus() {
+    println(
+        "\nApp: $APP " +
+                "\nversion: $VERSION\n"
+    )
+
     println("\nPilih menu -> (A)dd todo | (R)emove todo | (C)lear todo | (S)how todos | (E)xit program: ")
     var menus = readLine()
-    when (menus) {
-        "A" -> addTodo()
-        "R" ->  {
+    when (menus?.lowercase(Locale.getDefault())) {
+        "a", "add todo" -> addTodo()
+        "r", "remove todo" -> {
             println("Masukkan nomer todo yang akan di hapus:")
             removeTodo(readLine()!!.toInt())
         }
-        "C" -> clearTodo()
-        "S" -> showTodo()
-        "E" -> exitProcess(0)
+        "c", "clear todos" -> clearTodo()
+        "s", "show todo" -> showTodo()
+        "e", "exit", "exit program" -> exitProcess(0)
         else -> {
             showMenus()
         }
@@ -33,35 +38,32 @@ fun addTodo(){
     val todo = readLine()
     val sdf = SimpleDateFormat("dd/M/yyyy hh:mm:ss")
     val time = sdf.format(Date())
-    todolists.add("$todo | $time")
-    println(getInformation())
+    todolist.add("$todo | $time")
+    println("Berhasil menambahkan '${todolist.get(todolist.lastIndex)}'")
     showMenus()
 }
 
 fun showTodo(){
+    if (todolist.size <= 0){
+        println("Todolist is empty")
+        return
+    }
     println("\nTodo(s):")
     var i = 1
-    for (todo in todolists){
+    for (todo in todolist){
         println("${i++}. $todo")
     }
-    println(getInformation())
     showMenus()
 }
 
 fun clearTodo(){
-    todolists.clear()
-    println("Berhasil menghapus todolists!")
-    println(getInformation())
+    todolist.clear()
+    println("Berhasil menghapus todolist!")
     showMenus()
 }
 
 fun removeTodo(i:Int){
-    println("Menghapus ( ${todolists.get(i)} ) dari Todolists")
-    todolists.removeAt(i)
-    println(getInformation())
+    println("Menghapus ( ${todolist.get(i)} ) dari todolist")
+    todolist.removeAt(i)
     showMenus()
-}
-
-fun getInformation(): String {
-    return "\nApp: $APP" + "\nVersion: $VERSION"
 }
